2f56a6f9cf3406f8588e708fd372c9e652dffb00
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2022-04-21 17:33:07 -0700

    chore: add badge (#1)
    * chore: projen basics
    
    * chore: add badge to README
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,21 @@
 const { clickupCdk } = require('@time-loop/clickup-projen');
+
+const bundledDeps = [];
+const peerDeps = [];
+
 const project = new clickupCdk.ClickUpCdkConstructLibrary({
-  author: 'Andrew Hammond',
-  authorAddress: '445764+ahammond@users.noreply.github.com',
+  name: 'cdk-user-with-access-key',
+
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  devDeps: ['@time-loop/clickup-projen'],
-  name: 'cdk-user-with-access-key',
-  repositoryUrl: 'https://github.com/445764+ahammond/cdk-user-with-access-key.git',
+  licensed: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  bundledDeps,
+  deps: [...bundledDeps, 'cdk-iam-floyd'],
+  devDeps: [...peerDeps, '@time-loop/clickup-projen'],
+
+  repositoryUrl: '', // leverage default
+  authorName: '', // leverage default
+  authorAddress: '', // leverage default
 });
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@time-loop/cdk-user-with-access-key.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@time-loop/cdk-user-with-access-key.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@time-loop/cdk-user-with-access-key'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@time-loop/cdk-user-with-access-key.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@time-loop/cdk-user-with-access-key.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
