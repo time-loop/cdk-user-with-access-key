01aab272880a010a11859764254866f87c58401b
ClickUp CI/CD <93961939+cu-infra-svc-git@users.noreply.github.com>
2024-02-21 17:45:58 -0700

    chore(deps): update dependency @time-loop/clickup-projen to v1 Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

---
+++ .projenrc.ts
@@ -0,0 +1,21 @@
+import { clickupCdk } from '@time-loop/clickup-projen';
+
+const project = new clickupCdk.ClickUpCdkConstructLibrary({
+  name: 'cdk-user-with-access-key',
+
+  cdkVersion: '2.23.0',
+  defaultReleaseBranch: 'main',
+  licensed: true,
+  projenrcTs: true,
+
+  deps: [
+    'multi-convention-namer',
+    // 'source-map@^0.7.4', // CLK-393642 and https://github.com/mozilla/source-map/issues/454
+  ],
+  devDeps: ['@time-loop/clickup-projen'],
+
+  repositoryUrl: '', // leverage default
+  author: '', // leverage default
+  authorAddress: '', // leverage default
+});
+project.synth();
