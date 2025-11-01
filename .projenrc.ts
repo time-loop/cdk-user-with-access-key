1b3b138b41ce8a9e8d12802941d9c1a97ac44dd6
ClickUp CI/CD <93961939+cu-infra-svc-git@users.noreply.github.com>
2025-10-31 21:28:36 -0600

    fix(deps): upgrade projen (#33)
    * fix(deps): upgrade projen
    
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/time-loop/cdk-user-with-access-key/actions/runs/18988343078
    
    ------
    
    *Automatically created by projen via the "update-projen-main" workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * chore(projen): remove deprecated packageManager configuration
    
    * run projen
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Andrew Hammond <445764+ahammond@users.noreply.github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,22 +1,16 @@
 import { clickupCdk } from '@time-loop/clickup-projen';
-import { javascript } from 'projen';
 
 const project = new clickupCdk.ClickUpCdkConstructLibrary({
   name: 'cdk-user-with-access-key',
-
   cdkVersion: '2.23.0',
   defaultReleaseBranch: 'main',
   licensed: true,
   projenrcTs: true,
-  packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion: '9',
-
   deps: [
     'multi-convention-namer',
     // 'source-map@^0.7.4', // CLK-393642 and https://github.com/mozilla/source-map/issues/454
   ],
   devDeps: ['@time-loop/clickup-projen'],
-
   repositoryUrl: '', // leverage default
   author: '', // leverage default
   authorAddress: '', // leverage default
